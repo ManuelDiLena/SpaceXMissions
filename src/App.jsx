7d19f168ec2b646d889fb1b2432439d0f46da33a
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-22 18:48:14 +0100

    Getting data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-export function App() {
+import { useState, useEffect } from 'react';
+import * as API from './service/launches';
+import logo from './assets/logo-spacex.png';
+
+function App() {
+
+    const [launches, setLaunches] = useState([])
+
+    useEffect(() => {
+        API.getAllLaunches()
+            .then(setLaunches)
+    }, [])
+
     return (
-        <div>Hello World</div>
+        <div>
+            <img src={logo} width={300} />
+            <h1>SpaceX Launches</h1>
+            <ul>
+                {
+                    launches.map(launch => (
+                        <li key={launch.id}>
+                            {launch.name}: {launch.date_utc}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     ) 
 }
+
+export default App;
 
---
+++ src/service/launches.js
@@ -0,0 +1,27 @@
+const API_URL = 'https://api.spacexdata.com/v5';
+
+// Function to get all data from API
+export async function getAllLaunches() {
+    try {
+        const res = await fetch(`${API_URL}/launches`)
+        const data = await res.json()
+
+        return data
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+// Function to get a specific data
+export async function getLaunchByFlightNumber(flightNumber) {
+    try {
+        const res = await fetch(`${API_URL}/launches/${flightNumber}`)
+        const data = await res.json()
+
+        return data
+
+    } catch (e) {
+        console.error(e)
+    }
+}
